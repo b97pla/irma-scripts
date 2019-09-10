9abe04cb389e07561129ddfe3bd69b183fc30c53
Matilda Åslin <matilda.aslin@medsci.uu.se>
2019-09-10 14:06:36 +0200

    Apply suggestions from code review
    Co-Authored-By: Pontus Larsson <b97pla@users.noreply.github.com>

--- run_multiqc_bp_qc.sh
+++ run_multiqc_bp_qc.sh
@@ -4,8 +4,12 @@
 # A simple wrapper for the multiqc command in INS-22349
 # Usage: bash /proj/ngi2016001/private/scripts/run_multiqc_bp_qc.sh /proj/ngi2016001/nobackup/NGI/ANALYSIS/<project>
 #
-
+#SBATCH -A ngi2016001
+#SBATCH -n 8
+#SBATCH -t 05:00:00
+#SBATCH -J run_multiqc_bp_qc
+#SBATCH -o run_multiqc_bp_qc.%j.out
 PROJECT_PATH=$1
 
-sbatch -A ngi2016001 -n 8 -t 05:00:00 --wrap "multiqc --template default --config /proj/ngi2016001/private/conf/multiqc_config.yaml --title $(basename $PROJECT_PATH) --filename $(basename $PROJECT_PATH)_$(date +%y%m%d) --outdir multiqc_ngi --data-format json --zip-data-dir --no-push $PROJECT_PATH"
+multiqc --template default --config /proj/ngi2016001/private/conf/multiqc_config.yaml --title "$(basename $PROJECT_PATH)" --filename "$(basename $PROJECT_PATH)_$(date +%y%m%d)" --outdir multiqc_ngi --data-format json --zip-data-dir --no-push "$PROJECT_PATH"
 
