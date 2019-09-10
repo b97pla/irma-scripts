d8f1bda3fa110458235273c8232706fad4440ab0
b97pla <pontus.larsson@medsci.uu.se>
2019-09-10 11:08:58 +0200

    fixed md5-calculation when not invoking from parent directory

--- deliver_project_to_user.sh
+++ deliver_project_to_user.sh
@@ -9,7 +9,6 @@ PROJECT=$1
 EMAIL=$2
 SENSITIVE="--$3"
 MEMBERS="$4"
-PROJPATH=/proj/ngi2016001/incoming/GENOTYPING/$PROJECT
 
 # display usage instructions when invoked without arguments
 if [[ -z "$PROJECT" ]]
@@ -33,8 +32,20 @@ then
   done
 fi
 
+# set up paths
+GENOTYPEDIR="/proj/ngi2016001/incoming/GENOTYPING"
+STAGINGDIR="$GENOTYPEDIR/staging"
+SUPRCREDS="$GENOTYPEDIR/supr_creds.txt"
+PROJPATH="$GENOTYPEDIR/$PROJECT"
+LOGFILE=${0}.log
+TSTAMP=$(date +%s)
+MD5FILE="$GENOTYPEDIR/${PROJECT}.${TSTAMP}.md5"
+ORIGINAL_CWD="$(pwd)"
+
+# make the genotypedir the cwd
+cd "$GENOTYPEDIR"
+
 # File containing the SUPR credentails
-SUPRCREDS=/proj/ngi2016001/incoming/GENOTYPING/supr_creds.txt
 URL=$(cut -f 1 "$SUPRCREDS")
 USR=$(cut -f 2 "$SUPRCREDS")
 PASS=$(cut -f 3 "$SUPRCREDS")
@@ -45,22 +56,22 @@ CMD=$(echo /lupus/ngi/production/latest/sw/anaconda/envs/ugc_delivery_script/bin
 --supr_url $URL \
 --supr_api_user $USR \
 --supr_api_key $PASS \
---staging_area /proj/ngi2016001/incoming/GENOTYPING/staging \
+--staging_area $STAGINGDIR \
 --path $PROJPATH \
 --project $PROJECT \
 --email $EMAIL \
 $SENSITIVE $MEMBER_ARG)
 
 # Calculate MD5 sums of all delivered files
-TSTAMP=$(date +%s)
-MD5FILE=${PROJPATH}.${TSTAMP}.md5
 find $PROJECT -type f -exec md5sum {} \; > $MD5FILE
 mv $MD5FILE $PROJPATH/
 
 # Log the delivery
-LOGFILE=${0}.log
 MSG="$TSTAMP  "$(date -d @$TSTAMP --utc +"%Y-%m-%dT%H:%M:%SZ")"  $(hostname)  ${USER}  ${PROJPATH}/$(basename $MD5FILE)  "$(echo $CMD |sed -re "s/$PASS/**************/")
 echo "$MSG" >> $LOGFILE
 
 # Execute the command
 $CMD
+
+# change the cwd back
+cd "$ORIGINAL_CWD"
