856b99ef9245703787444c53923403c488f37c8a
b97pla <pontus.larsson@medsci.uu.se>
2021-04-14 14:58:57 +0200

    trying to avoid some hard-coded paths

--- make_nf_run_script.py
+++ make_nf_run_script.py
@@ -4,31 +4,47 @@ import argparse
 parser = argparse.ArgumentParser(description='Generate run script for nextflow pipelines')
 parser.add_argument('--project', required=True, help='Project name')
 parser.add_argument('--genome', required=True, help='Reference genome, e.g. GRCm38')
-parser.add_argument('--pipeline', required=True, help='Reference genome, e.g. methylseq')
+parser.add_argument('--pipeline', required=True, choices=["rnaseq", "methylseq"], help='Analysis pipeline, e.g. methylseq')
+parser.add_argument('--base-path', default=os.path.join("/proj", "ngi2016001", "nobackup", "NGI"), help='Path to the folder containing the ANALYSIS and DATA subfolders (default: %(default)s)')
 
 args = parser.parse_args()
 project = args.project
 genome = args.genome
 pipeline = args.pipeline
+base_path = args.base_path
 
-analysis_path = "/proj/ngi2016001/nobackup/NGI/ANALYSIS/"
-scripts_path = os.path.join(analysis_path, project, "scripts")
-template_path = "/lupus/ngi/production/latest/sw/upps_standalone_scripts/run_script_templates"
+analysis_path = os.path.join(base_path, "ANALYSIS")
+data_path = os.path.join(base_path, "DATA")
+project_path = os.path.join(analysis_path, project)
+scripts_path = os.path.join(project_path, "scripts")
+logs_path = os.path.join(project_path, "logs")
+self_path = os.path.dirname(os.path.realpath(__file__))
+template_path = os.path.join(self_path, "run_script_templates")
+config_path = os.path.join(self_path, "config", "genomes_GRCh38_gencode.config")
+extra_args = ""
 
-# Create scripts folder (if not already created)
-os.system("mkdir -p {}".format(scripts_path))
+# Create log and scripts folder (if not already created)
+for d in [scripts_path, logs_path]:
+  os.system(f"mkdir -p {d}")
 
 # Handle gencode for GRCh38
-if (pipeline == 'rnaseq' and genome == 'GRCh38'):
-  template_name = 'rnaseq_GRCh38_gencode'
-else:
-  template_name = pipeline
+if pipeline == 'rnaseq' and genome == 'GRCh38':
+  extra_args = "--gencode"
 
 # Copy template to scripts folder
-os.system("cp {}/{}_template {}/run_analysis.sh".format(template_path,template_name, scripts_path))
+os.system(f"cp {template_path}/{pipeline}_template {scripts_path}/run_analysis.sh")
 
 # Add project and genome to template
-os.system("sed -i -e s/PROJECT/{}/g -e s/GENOME/{}/g {}/run_analysis.sh".format(project, genome, scripts_path))
+sed_cmd = "gsed -i"
+for srch, rplc in [
+  ("_PROJECT_", project),
+  ("_GENOME_", genome),
+  ("_ANALYSISDIR_", analysis_path),
+  ("_DATADIR_", data_path),
+  ("_CONFIG_", f"-c {config_path}"),
+  ("_EXTRAARGS_", f"{extra_args}")]:
+  sed_cmd = f"{sed_cmd} -e 's#{srch}#{rplc}#g'"
 
-print("{}/run_analysis.sh has been generated. Good luck with the analysis!".format(scripts_path))
+os.system(f"{sed_cmd} {scripts_path}/run_analysis.sh")
 
+print(f"{scripts_path}/run_analysis.sh has been generated. Good luck with the analysis!")
